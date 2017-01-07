0ee47f5e1a2c669648ffc55ba26a991fc14715ea
BHAVIN1011 <patelbhavin3756@gmail.com>
2017-01-07 13:23:22 -0800

    Quiz using angularjs.
    In this project I work on HTML/5, CSS/3, JavaScript, AngularJS,
    Bootstrap, JSON.

---
+++ myQuiz/js/quiz.js
@@ -0,0 +1,59 @@
+(function(){
+
+	var app = angular.module('myQuiz', []);
+    
+    app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+        
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
+        
+        $http.get('quiz_data.json').then(function(quizData){
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;            
+        }); 
+        
+        $scope.selectAnswer = function(qIndex,aIndex){
+            //alert( qIndex + 'and' + aIndex );
+            var questionState = $scope.myQuestions[qIndex].questionState;
+            
+            if( questionState != 'answered' ){
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                var correctAnswer = $scope.myQuestions[qIndex].correct;
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+                
+                if( aIndex === correctAnswer ){
+                    $scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score += 1;
+                }else{
+                    $scope.myQuestions[qIndex].correctness = 'incorrect';
+                }
+                $scope.myQuestions[qIndex].questionState = 'answered'
+            }
+            $scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(2);
+        }
+        
+        $scope.isSelected = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;     
+        }
+        $scope.isCorrect = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;     
+        }
+        
+        $scope.selectContinue = function(){
+            return $scope.activeQuestion += 1;
+        }
+        
+        $scope.createShareLinks = function(percentage){
+            var url = 'http://codifydesign.com';
+            
+            var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my quiz score!&amp;body=I scored a '+percentage+'% on this quiz about Saturn. Try to beat my score at '+url+'">Email a friend</a>';
+            var twitterLink = '<a class="btn twitter" target="_blank" href="https://twitter.com/share?text=I scored a '+percentage+'% on this quiz about Saturn. Try to beat my score at&amp;hashtags=SaturnQuiz&amp;url='+url+'">Tweet your score</a>';
+            var newMarkup = emailLink + twitterLink;
+            return $sce.trustAsHtml(newMarkup);            
+        }    
+        
+    }]);
+
+})();
